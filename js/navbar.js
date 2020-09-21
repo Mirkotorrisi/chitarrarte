2d3bb39f2c2e6934ebdf8e4ad9ea5e86b0bdbc0e
Mirkotorrisi <mirko.torrisi92@gmail.com>
2020-09-21 13:16:16 +0200

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,25 @@
-var navLink = document.querySelector(".nav-link");
-var navbar = document.getElementById("subject");
+var navList = document.getElementById("nav-list");
+var navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".dropdown-toggle");
 
-navLink.onclick = changeColor;
-
-function changeColor() {
-  navbar.classList.add("newBackground");
-}
-
-window.addEventListener("click", function (e) {
+window.onclick = (e) => {
   if (!navbar.contains(e.target)) {
     navbar.classList.remove("newBackground");
+    removeAllShow();
+  } else navbar.classList.add("newBackground");
+};
+
+for (const link of links) {
+  link.onclick = () => {
+    removeAllShow();
+    link.parentElement.classList.add("show");
+    link.parentElement.children[1].classList.add("show");
+  };
+}
+
+const removeAllShow = () => {
+  for (let i = 0; i < navList.children.length; i++) {
+    navList.children[i].classList.remove("show");
+    navList.children[i].children[1].classList.remove("show");
   }
-});
+};
